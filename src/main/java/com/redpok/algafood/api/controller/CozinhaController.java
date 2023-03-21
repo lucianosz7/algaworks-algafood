c3ce65eddfa5513081231611fbe171379bf4655b
Luciano Santos <luciano_z7@hotmail.com>
2023-03-20 21:40:18 -0300

    Throwing ResponseStatusException type exceptions

--- src/main/java/com/redpok/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/redpok/algafood/api/controller/CozinhaController.java
@@ -16,8 +16,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebInputException;
 
-import com.redpok.algafood.domain.exception.EntidadeEmUsoException;
 import com.redpok.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.redpok.algafood.domain.model.Cozinha;
 import com.redpok.algafood.domain.repository.CozinhaRepository;
@@ -69,16 +70,24 @@ public class CozinhaController {
 		return ResponseEntity.notFound().build();
 	}
 	
+	/*
+	 * @DeleteMapping("/{cozinhaId}") public ResponseEntity<Cozinha>
+	 * remover(@PathVariable Long cozinhaId) { try {
+	 * cadastroCozinha.excluir(cozinhaId);
+	 * 
+	 * return ResponseEntity.noContent().build(); }catch
+	 * (EntidadeNaoEncontradaException e) { return
+	 * ResponseEntity.notFound().build(); }catch (EntidadeEmUsoException e) { return
+	 * ResponseEntity.status(HttpStatus.CONFLICT).build(); } }
+	 */
+	
 	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-		try {
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {
+		try {			
 			cadastroCozinha.excluir(cozinhaId);
-		
-			return ResponseEntity.noContent().build();
-		}catch (EntidadeNaoEncontradaException e) {			
-			return ResponseEntity.notFound().build();		
-		}catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}catch (EntidadeNaoEncontradaException e){
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 		}
 	}
 		
--- src/main/java/com/redpok/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/redpok/algafood/domain/service/CadastroCozinhaService.java
@@ -25,7 +25,7 @@ public class CadastroCozinhaService {
 			cozinhaRespository.deleteById(cozinhaId);
 		}catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
+			  		String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
 		}catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
